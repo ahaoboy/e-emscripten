4a956bd8cd454761d4e5f94ff2b7e32cf159138e
阿豪boy <504595380@qq.com>
2021-11-15 21:17:44 +0800

    fix: type

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,42 @@
-export type DataMap<R extends any> = R extends Emscripten.JSType
+// https://emscripten.org/docs/porting/connecting_cpp_and_javascript/Interacting-with-code.html
+type StringToType<R extends any> = R extends Emscripten.JSType
   ? {
       number: number;
       string: string;
-      array: number;
+      array: number | Array<number | string | boolean> | Uint8Array | Int8Array;
       boolean: boolean;
+      null: null;
     }[R]
   : never;
 
-export type CvtArg<T extends readonly Emscripten.JSType[]> = Extract<
+type ArgsToType<T extends Array<Emscripten.JSType | null>> = Extract<
   {
-    [P in keyof T]: DataMap<T[P]>;
+    [P in keyof T]: StringToType<T[P]>;
   },
   any[]
 >;
 
-type CvtReturn<R extends Emscripten.JSType | null> = R extends null
+type ReturnToType<R extends Emscripten.JSType | null> = R extends null
   ? null
-  : DataMap<Exclude<R, null>>;
+  : StringToType<Exclude<R, null>>;
 
 export declare function cwrap<
-  I extends readonly Emscripten.JSType[] | [],
+  I extends Array<Emscripten.JSType | null> | [],
   R extends Emscripten.JSType | null
 >(
   ident: string,
   returnType: R,
   argTypes: I,
   opts?: Emscripten.CCallOpts
-): (...arg: CvtArg<I>) => CvtReturn<R>;
+): (...arg: ArgsToType<I>) => ReturnToType<R>;
 
 export declare function ccall<
-  I extends readonly Emscripten.JSType[] | [],
+  I extends Array<Emscripten.JSType | null> | [],
   R extends Emscripten.JSType | null
 >(
   ident: string,
   returnType: R,
   argTypes: I,
-  args: CvtArg<I>,
+  args: ArgsToType<I>,
   opts?: Emscripten.CCallOpts
-): CvtReturn<R>;
+): ReturnToType<R>;
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -3,6 +3,7 @@ interface WasmModule extends EmscriptenModule {
   cwrap: typeof cwrap;
   ccall: typeof ccall;
 }
+
 describe("blah", () => {
   it("works", () => {
     let wasmModul: WasmModule;
@@ -10,15 +11,19 @@ describe("blah", () => {
       "string",
       "string",
       "number",
-    ] as const);
+    ]);
     const f2 = wasmModul.ccall(
       "test",
       "boolean",
-      ["string", "string", "number"] as const,
+      ["string", "string", "number"],
       ["1", "2", 2]
     );
     const a = f1("1", "1", 1);
     expect(typeof f2).toBe("number");
     expect(typeof a).toBe("boolean");
+    const int_sqrt = cwrap("int_sqrt", null, ["number"]);
+    int_sqrt(0);
+    const int_sqrt_number = cwrap("int_sqrt", "number", ["number"]);
+    int_sqrt_number(0)
   });
 });
